36510025183ba0f8ea79d2f981761c5c8f6577fd
minhyeong7 <163122988+minhyeong7@users.noreply.github.com>
2026-01-03 00:11:43 +0900

    MVP검증테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-export default function App() {
+import { useState } from "react";
+
+function App() {
+  const [text, setText] = useState("");
+
+  const send = async () => {
+    await fetch("http://localhost:8080/api/test", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content: text }),
+    });
+    alert("전송 완료");
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind 정상 동작 🎉
-      </h1>
+    <div >
+      <input className="border"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button className="border" onClick={send}>저장</button>
     </div>
-  )
+  );
 }
+
+export default App;
+
